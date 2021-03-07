11fe595ef977895876f0a907642911841e784024
Lars Pickelin <lars.pickelin2@vaillant-group.com>
2021-03-07 09:01:55 +0100

    remove :

--- pkg/mqtt/mqtt_client.go
+++ pkg/mqtt/mqtt_client.go
@@ -10,7 +10,7 @@ var BrokerClient mqtt.Client
 func Connect(broker string, userName string, password string, clientId string) {
 	opts := mqtt.NewClientOptions().AddBroker(broker).SetUsername(userName).SetPassword(password).SetClientID(clientId)
 
-	BrokerClient := mqtt.NewClient(opts)
+	BrokerClient = mqtt.NewClient(opts)
 	if token := BrokerClient.Connect(); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error().Error())
 	}
