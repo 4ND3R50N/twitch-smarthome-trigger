8b51016f01dc3d39199e0d85ed92fe5404b16e8a
Lars Pickelin <lars.pickelin2@vaillant-group.com>
2021-03-07 09:08:07 +0100

    Fix topic names

--- internal/twitch-chat-smarthome-trigger/smarthome/light/change_light_colors.go
+++ internal/twitch-chat-smarthome-trigger/smarthome/light/change_light_colors.go
@@ -2,12 +2,9 @@ package light
 
 import (
 	"errors"
-	"fmt"
+	"github.com/4ND3R50N/twitch-chat-smarthome-trigger/pkg/mqtt"
 	"github.com/4ND3R50N/twitch-chat-smarthome-trigger/pkg/project"
 	"io/ioutil"
-	"strconv"
-
-	"github.com/4ND3R50N/twitch-chat-smarthome-trigger/pkg/mqtt"
 )
 
 func ChangeColor(color Color) (string, error) {
@@ -19,20 +16,16 @@ func ChangeColor(color Color) (string, error) {
 		if err != nil {
 			return "", errors.New("unable to load file for color " + Blue.String())
 		}
-		fmt.Println("Until here everything works! Mqtt connected: " + strconv.FormatBool(mqtt.BrokerClient.IsConnected()))
 		// todo: adjust topic name
 		// todo: store smarthome devices in seperate file
 		// workstation-01
-		fmt.Println("Send message payload" + string(file))
-		rs1 := mqtt.BrokerClient.Publish("homeassist/0x00158d00038ea806/set", 1, false, file)
+		rs1 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038ea806/set", 1, false, file)
 		rs1.Wait()
 		// workstation-02
-		fmt.Println("Send message")
-		rs2 := mqtt.BrokerClient.Publish("homeassist/0x00158d00038ebb56/set", 1, false, file)
+		rs2 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038ebb56/set", 1, false, file)
 		rs2.Wait()
 		// workstation-03
-		fmt.Println("Send message")
-		rs3 := mqtt.BrokerClient.Publish("homeassist/0x00158d00038eca7c/set", 1, false, file)
+		rs3 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038eca7c/set", 1, false, file)
 		rs3.Wait()
 		return "Room color changed to " + Blue.String(), nil
 	}
