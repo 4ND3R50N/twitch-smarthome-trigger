76afa78dd2f1197ed946dbc9787954b55c1d1821
Lars Pickelin <lars.pickelin2@vaillant-group.com>
2021-03-07 09:23:48 +0100

    Optimize modul

--- internal/twitch-chat-smarthome-trigger/smarthome/light/change_light_colors.go
+++ internal/twitch-chat-smarthome-trigger/smarthome/light/change_light_colors.go
@@ -8,27 +8,37 @@ import (
 )
 
 func ChangeColor(color Color) (string, error) {
-	if color == Blue {
-		// todo: make function for sending payload by color
-		// todo: add real payload
-		file, err := ioutil.ReadFile(project.Path + "/assets/smarthome/light/bright_blue.json")
 
+	var payload []byte
+	var err error
+
+	switch color {
+	case Blue:
+		payload, err = ioutil.ReadFile(project.Path + "/assets/smarthome/light/bright_blue.json")
 		if err != nil {
 			return "", errors.New("unable to load file for color " + Blue.String())
 		}
-		// todo: adjust topic name
-		// todo: store smarthome devices in seperate file
-		// workstation-01
-		rs1 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038ea806/set", 1, false, file)
-		rs1.Wait()
-		// workstation-02
-		rs2 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038ebb56/set", 1, false, file)
-		rs2.Wait()
-		// workstation-03
-		rs3 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038eca7c/set", 1, false, file)
-		rs3.Wait()
-		return "Room color changed to " + Blue.String(), nil
+	case Orange:
+		payload, err = ioutil.ReadFile(project.Path + "/assets/smarthome/light/bright_orange.json")
+		if err != nil {
+			return "", errors.New("unable to load file for color " + Blue.String())
+		}
+	default:
+		return "Your color is not implemented yet!", nil
 	}
+	setLightState(payload)
+	return "Successfully changed color", nil
+}
 
-	return "Your color is not implemented yet!", nil
+func setLightState(payload []byte) {
+	// todo: store smarthome devices in seperate file
+	// workstation-01
+	rs1 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038ea806/set", 1, false, payload)
+	rs1.Wait()
+	// workstation-02
+	rs2 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038ebb56/set", 1, false, payload)
+	rs2.Wait()
+	// workstation-03
+	rs3 := mqtt.BrokerClient.Publish("homeassistant/0x00158d00038eca7c/set", 1, false, payload)
+	rs3.Wait()
 }
