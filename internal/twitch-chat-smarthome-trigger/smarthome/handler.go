63985aa9152a925dd7c07be7f458b96ce99c8fb2
Lars Pickelin <lars.pickelin2@vaillant-group.com>
2021-03-07 08:17:52 +0100

    Fix enum call

--- internal/twitch-chat-smarthome-trigger/smarthome/handler.go
+++ internal/twitch-chat-smarthome-trigger/smarthome/handler.go
@@ -11,9 +11,10 @@ const lightCommand = "change_lights " + commandSeparator
 
 func HandleMessage(message string) string {
 	if strings.HasPrefix(message, lightCommand) {
+		fmt.Println("DEBUG: message: " + message)
 		command := strings.Split(message, commandSeparator)
 		fmt.Println("DEBUG: command 1: " + command[1])
-		status, err := light.ChangeColor(light.Color(command[1]).ByString(command[1]))
+		status, err := light.ChangeColor(light.Color(command[1]))
 
 		if err != nil {
 			fmt.Printf(err.Error())
