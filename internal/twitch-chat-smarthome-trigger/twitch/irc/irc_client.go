1a33047783540d7bae8463a3d4b0de697fabd2dd
Lars Pickelin <lars.pickelin2@vaillant-group.com>
2021-03-15 19:40:59 +0100

    push

--- internal/twitch-chat-smarthome-trigger/twitch/irc/irc_client.go
+++ internal/twitch-chat-smarthome-trigger/twitch/irc/irc_client.go
@@ -4,4 +4,4 @@ import "github.com/gempir/go-twitch-irc/v2"
 
 // todo: make env vars to for these values
 // todo: think about putting this back to main
-var Client = twitch.NewClient("juel_bot", "oauth:lw6qgrh0v5li1gofit5onsmkvmfeoy")
+var Client = twitch.NewClient("juel_bot", "oauth:8j2fs0b61zwdc4i15srbd2txcolnn8")
