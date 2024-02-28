1f2ef078f8acc085e1736884d1d9b2f3b4ed052d
Lars P <lars.pickelin@hetzner-cloud.de>
2024-02-28 22:15:25 +0100

    test

--- internal/smarthome-trigger/callbacks.go
+++ internal/smarthome-trigger/callbacks.go
@@ -18,14 +18,13 @@ type SceneData struct {
 
 func (s *Service) homeAssistantCommandParser(message string) (*string, error) {
 	// parse command
-	fmt.Println("test")
 	command, content, err := ParseCommands(message)
 	if err != nil {
 		// Only early return, since most of the messages are no commands.
 		return nil, nil
 	}
-	fmt.Println("test")
 	if command == LightCommand {
+		fmt.Println("test1")
 		request := SceneBodyRequest{
 			Service: "scene.turn_on",
 			ServiceData: SceneData{
@@ -36,21 +35,25 @@ func (s *Service) homeAssistantCommandParser(message string) (*string, error) {
 		if err != nil {
 			return nil, err
 		}
+		fmt.Println("test2")
 		req, err := http.NewRequest("POST",
 			s.homeAssistantURL+"/api/services/scene", bytes.NewBuffer(byteRequest))
 		if err != nil {
 			return nil, err
 		}
+		fmt.Println("test3")
 		req.Header.Add("content-type", "application/json")
 		req.Header.Add("Authorization", s.homeAssistantToken)
 		response, err := s.apiClient.Do(req)
 		if err != nil {
 			return nil, err
 		}
+		fmt.Println("test4")
 		defer response.Body.Close()
 		if response.Status != "200" {
 			return nil, err
 		}
 	}
+	fmt.Println("test5")
 	return nil, nil
 }
